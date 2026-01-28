e7bd8f2ca144f3145431abbd922ff7366c410788
cppdecl <cppdecl@gmail.com>
2026-01-29 01:52:45 +0800

    Updated original Navbar code to a component

---
+++ app/components/Navbar/Navbar.tsx
@@ -0,0 +1,43 @@
+import "./Navbar.css";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark fixed-top">
+      <div className="container">
+        <a className="navbar-brand fw-bold" href="#home">
+          {">_<"}
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div
+          className="collapse navbar-collapse justify-content-end"
+          id="navbarNav"
+        >
+          <ul className="navbar-nav gap-3">
+            <li className="nav-item">
+              <a className="nav-link active" href="#home">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#portfolio">
+                Portfolio
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#contact">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
